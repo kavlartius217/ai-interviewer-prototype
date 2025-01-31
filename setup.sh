96bc6e0f0b15b6f27200a8eff9b35bfaba536839
kavlartius217 <kavlartius@gmail.com>
2025-01-31 18:06:55 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 
-# Download and install SQLite 3.39.0 (or latest stable version)
-mkdir -p $HOME/sqlite3
-cd $HOME/sqlite3
-wget https://www.sqlite.org/2022/sqlite-autoconf-3390000.tar.gz
-tar xzf sqlite-autoconf-3390000.tar.gz
-cd sqlite-autoconf-3390000
-./configure --prefix=$HOME/sqlite3
-make -j4
+# Set SQLite version
+SQLITE_VERSION=3410200  # This is SQLite 3.41.2 (or use the latest available)
+
+# Create a directory for the custom installation
+mkdir -p $HOME/sqlite
+cd $HOME/sqlite
+
+# Download and extract SQLite
+wget https://www.sqlite.org/2023/sqlite-autoconf-${SQLITE_VERSION}.tar.gz
+tar xzf sqlite-autoconf-${SQLITE_VERSION}.tar.gz
+cd sqlite-autoconf-${SQLITE_VERSION}
+
+# Compile SQLite
+./configure --prefix=$HOME/sqlite
+make -j$(nproc)
 make install
 
-# Add new SQLite to the PATH
-export PATH=$HOME/sqlite3/bin:$PATH
-export LD_LIBRARY_PATH=$HOME/sqlite3/lib:$LD_LIBRARY_PATH
+# Set environment variables for the new SQLite version
+export PATH=$HOME/sqlite/bin:$PATH
+export LD_LIBRARY_PATH=$HOME/sqlite/lib:$LD_LIBRARY_PATH
 
 # Verify the installed version
 sqlite3 --version
